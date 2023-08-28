a32bc458703286dbe3773e83a03b81a5581b4d20
micael.cruz <micael.cruz@DKT-CODIN-666>
2023-08-28 13:08:07 -0300

    refactor: otimizando confirmMessageService.js

--- src/services/confirmMessageService.js
+++ src/services/confirmMessageService.js
@@ -2,18 +2,10 @@ const responseConfirm = ['SIM', 'OK', 'S']
 const responseDeny = ['N', 'NAO']
 
 exports.confirm = function(text){
-    if(responseConfirm.includes(text.trim().toUpperCase()))
-    {
-        return true;
-    }
-    else return false;
+    return responseConfirm.includes(text.trim().toUpperCase())
 }
 
 exports.deny = function(text){
-    if(responseDeny.includes(text.trim().toUpperCase().replace('Ã','A')))
-    {
-        return true;
-    }
-    else return false;
+    return responseDeny.includes(text.trim().toUpperCase().replace('Ã','A'));
 }
 
